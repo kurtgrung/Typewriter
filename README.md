d3f44c82f2d998bb035e5d06acad486813b251a0
“Kurt Grüng” <114594062+kurtgrung@users.noreply.github.com>
2024-01-25 08:04:56 +0000

    Vue/Nuxt version 🟢
    Vue/Nuxt v3 added
    
    Readme updated

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ Javascript Typewriter Effect
 
 Vanilla Javascript Class & example. <br>
 React Typescript JSX component. 
+Vue/Nuxt version. <br>
 
 Vanilla <a target="_blank" href="https://jsfiddle.net/kurtgrung/oc9qwvp6/124/">Demo</a> <br>
 React <a target="_blank" href="https://typewriter-app.vercel.app">Demo</a>
---
+++ Vue/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <Typewriter :messages="messages" :pause="10" :typingSpeed="50" :cursorDelay="200" cursorSymbol="|" />
+    </div>
+</template>
+
+<script setup lang="ts">
+
+import Typewriter from './Typewriter.vue';
+
+const messages = [
+    'Typewriter :)',
+    'hello',
+    'this is a message :)',
+    '...and another message!',
+];
+
+</script>
