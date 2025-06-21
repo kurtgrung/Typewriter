8ab0bf54c1c65d5ed0c4847aad8cc45108636f24
Kurt Grüng <krgrung@gmail.com>
2025-06-21 20:13:41 +0100

    Refactor 🟢 Vue/Nuxt Styles Configurable
    Co-Authored-By: Kurt Grüng <114594062+kurtgrung@users.noreply.github.com>
    Co-Authored-By: Kurt Grüng <krgrung@gmail.com>

--- Vue/App.vue
+++ Vue/App.vue
@@ -1,18 +1,22 @@
 <template>
-    <div>
-        <Typewriter :messages="messages" :pause="10" :typingSpeed="50" :cursorDelay="200" cursorSymbol="|" />
-    </div>
+  <Typewriter 
+    :messages="messages" 
+    :pause="10" 
+    :typingSpeed="50" 
+    :cursorDelay="200" 
+    cursorSymbol="|"
+    :wrapperStyle="{ background: '#000', color: 'white', fontSize: '1.5em' }"
+    :cursorStyle="{ color: '#0f0', fontWeight: 'bold' }" 
+  />
 </template>
 
 <script setup lang="ts">
-
 import Typewriter from './Typewriter.vue';
 
 const messages = [
-    'Typewriter :)',
-    'hello',
-    'this is a message :)',
-    '...and another message!',
+  'Typewriter :)',
+  'hello',
+  'this is a message :)',
+  '...and another message!',
 ];
-
 </script>
